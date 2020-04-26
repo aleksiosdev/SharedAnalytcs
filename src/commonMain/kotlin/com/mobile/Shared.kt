da958b3a3622bbbfb5feafacb2177a225c83f495
Aleksandr <aleksios@manychat.com>
2020-04-26 20:02:26 +0300

    Base event type added

--- src/commonMain/kotlin/com/mobile/Shared.kt
+++ src/commonMain/kotlin/com/mobile/Shared.kt
@@ -2,9 +2,22 @@ package com.kurt.jokes.mobile
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.GlobalScope
 
-class Foo {
-    fun foo() : String {
-        GlobalScope.launch {}
-        return "Test"
-    } 
+public class Event {
+    val id: String
+    val name: String
+
+    public constructor(id: String, name: String) {
+        this.id = id
+        this.name = name
+    }
+}
+
+public enum class EventType {
+    LOGIN;
+
+    public fun event(): Event {
+        return when (this) {
+            LOGIN -> Event("1", "LOGIN")
+        }
+    }
 }
