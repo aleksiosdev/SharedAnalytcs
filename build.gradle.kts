3195962dd1ebe861d0a44cd8be5e31aedaf00387
Aleksandr <aleksios@manychat.com>
2020-04-27 14:41:13 +0300

    Change source set

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,6 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.Framework
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 val kotlinVersion = "1.3.72"
-version = "1.0.0"
-
-
 val ktorVersion = "1.3.2"
 val serializationVersion = "0.20.0"
 val coroutinesVersion = "1.3.3"
@@ -18,6 +15,9 @@ plugins {
     id("maven-publish")
 }
 
+group = "org.gradle.analytics"
+version = "1.1.1"
+
 repositories {
     mavenCentral()
     google()
@@ -25,20 +25,13 @@ repositories {
 }
 
  publishing {
-    publications {
-        create<MavenPublication>("SharedAnalytics") {
-            groupId = "org.gradle.analytics"
-            artifactId = "analyticsShared"
-            version = "1.0"
-        }
-    }
     repositories {
         maven {
             name = "SharedAnalytics"
             url = uri("https://maven.pkg.github.com/aleksiosdev/SharedAnalytcs")
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                username = "aleksiosdev" ?: System.getenv("USERNAME")
+                password = "700c778d3c8127d6b97ba8a36426ebf119eabfba" ?: System.getenv("TOKEN")
             }
         }
     }
@@ -64,27 +57,39 @@ kotlin {
         }
     }
 
-    jvm("android")
-
-    sourceSets["commonMain"].dependencies {
-        implementation("org.jetbrains.kotlin:kotlin-stdlib-common")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:$coroutinesVersion")
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-common:$serializationVersion")
-        implementation("io.ktor:ktor-client-json:$ktorVersion")
-        implementation("io.ktor:ktor-client-serialization:$ktorVersion")
-        implementation("io.ktor:ktor-client-core:$ktorVersion")
+    jvm("android") {
+         mavenPublication { 
+            artifactId = "SharedAnalytics"
+        }
     }
 
-    sourceSets["iosMain"].dependencies {
-        implementation("org.jetbrains.kotlin:kotlin-stdlib")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-native:$coroutinesVersion")
-        implementation("io.ktor:ktor-client-json-native:$ktorVersion")
-        implementation("io.ktor:ktor-client-serialization-native:$ktorVersion")
-        implementation("io.ktor:ktor-client-ios:$ktorVersion")
-    }
+     sourceSets {
+        commonMain {
+            dependencies {
+                implementation("org.jetbrains.kotlin:kotlin-stdlib-common")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:$coroutinesVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-common:$serializationVersion")
+                implementation("io.ktor:ktor-client-json:$ktorVersion")
+                implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+            }
+        }
 
-    sourceSets["androidMain"].dependencies {
-        implementation("org.jetbrains.kotlin:kotlin-stdlib")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-native:$coroutinesVersion")
-    }
+         val iosMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlin:kotlin-stdlib")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-native:$coroutinesVersion")
+                implementation("io.ktor:ktor-client-json-native:$ktorVersion")
+                implementation("io.ktor:ktor-client-serialization-native:$ktorVersion")
+                implementation("io.ktor:ktor-client-ios:$ktorVersion")
+            }
+        }
+
+       val androidMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlin:kotlin-stdlib")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-native:$coroutinesVersion")
+            }
+        }
+     }
 }
